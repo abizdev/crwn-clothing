61a22f21d428a40b2bf85f4f5baaea9cc1a6a347
Abdulaziz <abizlancer@gmail.com>
2023-09-30 13:00:47 +0500

    feat: add navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import Home from './routes/home/home.component'
 import { Routes, Route } from 'react-router-dom'
+
+import Home from './routes/home/home.component'
+import Navigation from './components/navigation/navigation.component'
+
 import './index.scss'
+
+const Shop = () => {
+  return <div>SHOP</div>
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   )
 }
---
+++ src/components/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+// import { ReactComponent as CrwnLogo } from '/src/assets/crown.svg'
+
+import './navigation.styles.scss'
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <nav className="navigation">
+        <Link className="logo-cotnainer" to='/'>
+          <img src="../../assets/crown.svg" alt="logo" className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to='/shop'>SHOP</Link>
+        </div>
+      </nav>
+      
+      <Outlet />
+    </Fragment>
+  )
+}
+
+export default Navigation
