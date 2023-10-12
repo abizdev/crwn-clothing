9fe49a419616b8e33ddb281a9ba2692d72916051
Abdulaziz <abizlancer@gmail.com>
2023-10-12 11:22:39 +0500

    feat: add checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './routes/home/home'
 import Authentication from './routes/authentication/authentication'
 import Navigation from './components/navigation/navigation.component'
 import Shop from './routes/shop/shop.component'
+import Checkout from './routes/checkout/checkout.component'
 
 import './index.scss'
 
@@ -15,6 +16,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   )
