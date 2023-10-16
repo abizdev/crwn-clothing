2b02bf7d589520e157578ef25e6adeee34b5b9c1
Abdulaziz <abizlancer@gmail.com>
2023-10-16 12:04:00 +0500

    fix: styles

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,18 +1,18 @@
-import './catetgory-item.style.scss'
+import './directory-item.style.scss'
 
-const CategoryItem = ({ category }) => {
+const DirectoryItem = ({ category }) => {
   const { id, title, imageUrl } = category
   return (
-    <div className="category-container" key={id}>
+    <div className="directory-item-container" key={id}>
       <div className='background-image' style={{
         backgroundImage: `url(${imageUrl})`
       }} />
-      <div className="category-body-container">
+      <div className="body">
         <h2>{title}</h2>
         <p>Shop Now</p>
       </div>
     </div>
   )
 }
-export default CategoryItem
+export default DirectoryItem
 
