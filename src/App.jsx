50599744e7ea21a9ea5f233891365e0bc9b555fc
Abdulaziz <abizlancer@gmail.com>
2023-10-27 10:48:27 +0500

    feat: add redux and user reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Routes, Route } from 'react-router-dom'
 
+
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from "./utils/firebase/firebase.utils";
 import Home from './routes/home/home'
 import Authentication from './routes/authentication/authentication'
 import Navigation from './components/navigation/navigation.component'
 import Shop from './routes/shop/shop.component'
 import Checkout from './routes/checkout/checkout.component'
+import { setCurrentUser } from './store/user/user.action'
+
 
 import './index.scss'
 
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if(user) {
+        createUserDocumentFromAuth(user)
+      }
+      dispatch(setCurrentUser(user))
+    })
+
+    return unsubscribe
+  }, [])
+
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import App from "./App.jsx";
 import { UserProvider } from "./context/user.context.jsx";
 import { CategoriesProvider } from "./context/categories.context.jsx";
 import { CartProvier } from "./context/cart.context.jsx";
+import { store } from "./store/store.js";
 
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvier>
-            <App />
-          </CartProvier>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvier>
+              <App />
+            </CartProvier>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
